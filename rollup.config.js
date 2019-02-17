268e767135d486047a355b9e83713c56a4d1982c
rogerbf <roger@bergfa.lk>
2019-02-17 10:25:12 +0100

    refactor

--- rollup.config.js
+++ rollup.config.js
@@ -3,35 +3,34 @@ import commonjs from "rollup-plugin-commonjs"
 import resolve from "rollup-plugin-node-resolve"
 import pkg from "./package.json"
 
-export default [
-  // browsers
-  {
-    input: `source/main.js`,
-    output: {
-      name: `MaskProperties`,
-      file: pkg.browser,
-      format: `umd`,
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      babel({
-        exclude: [ `node_modules/**` ],
-      }),
-    ],
+const browsers = {
+  input: `source/main.js`,
+  output: {
+    name: `MaskProperties`,
+    file: pkg.browser,
+    format: `umd`,
   },
-  // node and bundlers
-  {
-    input: `source/main.js`,
-    external: Object.keys(pkg.dependencies || {}),
-    output: [
-      { file: pkg.main, format: `cjs` },
-      { file: pkg.module, format: `es` },
-    ],
-    plugins: [
-      babel({
-        exclude: [ `node_modules/**` ],
-      }),
-    ],
-  },
-]
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: [ `node_modules/**` ],
+    }),
+  ],
+}
+
+const nodeAndBundlers = {
+  input: `source/main.js`,
+  external: Object.keys(pkg.dependencies || {}),
+  output: [
+    { file: pkg.main, format: `cjs` },
+    { file: pkg.module, format: `es` },
+  ],
+  plugins: [
+    babel({
+      exclude: [ `node_modules/**` ],
+    }),
+  ],
+}
+
+export default [ browsers, nodeAndBundlers ]
